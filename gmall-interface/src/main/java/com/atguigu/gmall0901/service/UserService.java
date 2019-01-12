4cd3e9623e10bfbd1e1ee15ae606d8c9727a07fd
cv <1209868150@qq.com>
2019-01-12 08:34:29 +0800

    项目01

---
+++ gmall-interface/src/main/java/com/atguigu/gmall0901/service/UserService.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall0901.service;
+
+import com.atguigu.gmall0901.bean.UserAddress;
+import com.atguigu.gmall0901.bean.UserInfo;
+
+import java.util.List;
+
+public interface UserService {
+    /**
+     * 查询所有用户
+     * @return
+     */
+    List<UserInfo> findall();
+
+    List<UserAddress> findUserAddressByUserId(String userId);
+
+}
